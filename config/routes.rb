787aeb4c4b2cd479d98b11d244e6b114156d97ec
tung <nguyenthanhtunghanuk14@gmail.com>
2018-07-15 23:23:46 +0700

    show and delete user

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   namespace :account do
-    resources :users
-    resources :products
-    resources :orders
+    resources :sessions, only: %i(new create destroy)
+    resources :users, only: %i(new create show edit update)
+    resources :products, only: %i(index show)
+    resources :orders, only: %i(show new create edit update destroy)
   end
 
   namespace :admin do
-    resources :users
-    resources :products
-    resources :categories
-    resources :orders
+    root "dashboard#home"
+    resources :users, only: %i(index destroy)
+    resources :products, only: %i(index new create edit update destroy)
+    resources :categories, only: %i(index new create edit update destroy)
+    resources :orders, only: %i(index destroy)
   end
 end
