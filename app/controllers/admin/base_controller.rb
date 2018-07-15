95f1d732685acbaa49e60209baeb11c549d3d9ea
tung <nguyenthanhtunghanuk14@gmail.com>
2018-07-15 23:23:46 +0700

    show and delete user

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,15 @@
 module Admin
   class BaseController < ApplicationController
+    layout "admin"
+
     before_action :authenticate_user!
+
+    # redirect to homepage if user is not logged in and not an admin
+    def authenticate_user!
+      # unless current_user.present? && current_user.admin?
+      #   flash.now.alert = "You are not an admin!"
+      #   redirect_to root_path
+      # end
+    end
   end
 end
