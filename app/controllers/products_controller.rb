43a93189a204a09595701f00a791e48b9a43c2bb
tung <nguyenthanhtunghanuk14@gmail.com>
2018-07-18 08:56:06 +0700

    display product detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :load_product, only: :show
+
   def index
     @products = Product.all.page(params[:page])
       .per(Settings.product.pagination)
+  end
+
+  def show; end
+
+  private
+
+  def load_product
+    @product = Product.find_by(id: params[:id])
+    redirect_to root_path, alert: t(:no_product) unless @product
   end
 end
